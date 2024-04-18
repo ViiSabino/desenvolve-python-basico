5924ad8db3117db11acb7215ef984ee23adcd7e9
Vincenth <163206275+ViiSabino@users.noreply.github.com>
2024-04-18 17:51:05 -0300

    Ex 2.2 - Tipos de dados

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,7 @@
+text = "o resultado é"
+a = 10
+b = 3.5
+c = a + b
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
---
+++ modulo2/aula2_questao3.py
@@ -0,0 +1,7 @@
+v1 = 10
+v2 = 20
+print("Valores iniciais:", v1, v2)
+v3 = v1
+v1 = v2
+v2 = v3
+print("Valores finais:", v1, v2)
